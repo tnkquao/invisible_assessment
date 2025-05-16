e4356ba627be5e2b51221f3d2e3f5744a6149307
Tarek Quao <tarek.quao@gmail.com>
2025-05-16 11:09:12 +0000

    implemented the Account functionality

---
+++ src/ATMSystem/Account.java
@@ -0,0 +1,55 @@
+package ATMSystem;
+
+public class Account {
+
+    private final String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public Account(String accountNumber, String accountHolderName) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = 0.0;
+    }
+
+    public Account(String accountNumber, String accountHolderName, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = initialBalance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance && amount > 0) {
+            balance -= amount;
+            System.out.println("Withdrew: $" + amount);
+        } else {
+            System.out.println("Insufficient balance or invalid amount.");
+        }
+    }
+    public void displayBalance() {
+        System.out.println("Current Balance: $" + balance);
+    }
+    
+}
